ba8ea5cebd9a52fe7146cf418215fe543119bbe0
Vivek <vivekkr9119@gmail.com>
2021-12-23 22:11:34 +0530

    Added underline design

--- load-meds.php
+++ load-meds.php
@@ -1,24 +1,21 @@
 <?php
 
-$username = "username";
-$password = "password@123";
-
-$conn = mysqli_connect("localhost", "root", "", "testing") or die("Connection Failed");
-$search_term = $_POST[meds];
-$sql = "SELECT distinct(meds) FROM students WHERE meds LIKE '%{$search_term}%'";
+$conn = mysqli_connect("localhost", "root", "password@123", "testing") or die("Connection Failed");
+$search_term = $_POST['meds'];
+$sql = "SELECT distinct(meds) FROM medicines WHERE meds LIKE '{$search_term}%'";
 $result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
-$output = "<ul>";
+$output = "<ul class="."list-group".">";
 if (mysqli_num_rows($result) > 0)
 {
     while ($row = mysqli_fetch_assoc($result))
     {
-        $output .= "<li>{$row['meds']}</li>";
+        $output .= "<li class="."list-group-item".">{$row['meds']}</li>";
     }
 }
 else
 {
-    $output .= "<li>No results</li>";
+    $output .= "<li class="."list-group-item".">No results</li>";
 }
 $output .= "</ul>";
-
+echo $output;
 ?>
